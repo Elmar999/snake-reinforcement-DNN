6392936ae376dc6a5dd8118a84a34a5ab1281e99
Elmar <baandicoot@gmail.com>
2020-05-05 18:23:03 +0400

    refactoring

--- config.py
+++
@@ -1,5 +0,0 @@
-
-
-MODEL_PATH = '/home/elmar/Documents/projects/rl_learning/snake_game/snake_model.h5'
-DATA_PATH = '/home/elmar/Documents/projects/rl_learning/snake_game/snake_data.npy'
-EPOCHS = 200
