8662c04635c4f30d46a0110828d93a8f9ca289df
Elmar <baandicoot@gmail.com>
2020-05-05 18:31:09 +0400

    refactoring

--- game/config.py
+++ game/config.py
@@ -1,5 +1,5 @@
+import os
 
-
-MODEL_PATH = '/home/elmar/Documents/projects/rl_learning/snake_game/snake_model.h5'
-DATA_PATH = '/home/elmar/Documents/projects/rl_learning/snake_game/snake_data.npy'
+MODEL_PATH = f'{os.getcwd()}' + '/snake_model.h5'
+DATA_PATH = f'{os.getcwd()}' + '/snake_data.npy'
 EPOCHS = 200
